eae9d4488af3c78091d5d772056e83209e3c9a69
Raphael-Alves <fernzull@gmail.com>
2025-02-16 15:44:42 -0300

    Add router.js and sucessfully config

--- backend/src/routes/router.js
+++ backend/src/routes/router.js
@@ -1,10 +1,18 @@
 import express from "express";
+import userController from "../controllers/userController.js";
 
 const router = express.Router();
 
-router.get('/', (req,res) =>{
-    console.log("Hello World");
-    res.status(200).json({ message: "Hello World! "})
+router.get("/", (req, res) => {
+    res.status(200).json({ message: "API is running!" });
 });
+
+router.get("/users", userController.getAllUsers);
+router.get("/users/:id", userController.getUserById);
+router.get("/users/login/:login", userController.getUserByLogin);
+router.get("/users/email/:email", userController.getUserByEmail);
+router.post("/users", userController.createUser);
+router.put("/users/:id", userController.updateUser);
+router.delete("/users/:id", userController.deleteUser);
 
 export default router;
